aa1e099c553e3f6ce00eed22a4eb7e99ee24c1b3
BENZABOSS <joyceben33@gmail.com>
2020-11-16 23:52:16 -0500

    Configuring ESLint and Prettier to work together.

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,13 +6,18 @@ module.exports = {
   extends: [
     'plugin:vue/essential',
     '@vue/airbnb',
-    '@vue/typescript/recommended',
+    '@vue/typescript',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended'
   ],
   parserOptions: {
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module',
     ecmaVersion: 2020,
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+
   },
 };
