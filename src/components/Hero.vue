6270ae98d189bd1484d8e497f412386245aba956
BENZABOSS <joyceben33@gmail.com>
2020-11-17 00:23:12 -0500

    Initial start with Header and Hero component

---
+++ src/components/Hero.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="hero" dark>
+    <v-img :src="require('../assets/golden_gate_0002.jpeg')" height="500px">
+      <div id="hero-gradient">
+        <v-container>
+          <div></div>
+        </v-container>
+      </div>
+    </v-img>
+  </div>
+</template>
+
+<style lang="scss">
+#hero-gradient {
+  background-image: linear-gradient(to right top, rgba(36, 36, 36, 0.9), rgba(25, 32, 72, 0.7));
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  padding-top: 64px;
+}
+</style>
+
+<script lang="ts">
+import Vue from 'vue';
+import Component from 'vue-class-component';
+
+@Component({})
+export default class Hero extends Vue {}
+</script>
---
+++ src/components/layout/Header.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-app-bar absolute flat color="transparent" dark>
+    <div>
+      <v-img
+        id="profile-pic"
+        alt="Vuetify Logo"
+        contain
+        :src="require('../../assets/profile_pic001.jpeg')"
+        transition="scale-transition"
+        width="54"
+      />
+    </div>
+    <h1 id="profile-name">Ben Joyce</h1>
+
+    <v-spacer></v-spacer>
+
+    <v-btn text>
+      <v-icon>mdi-face</v-icon>
+      <h3>About</h3>
+    </v-btn>
+    <v-btn text>
+      <v-icon>mdi-laptop</v-icon>
+      <h3>Projects</h3>
+    </v-btn>
+    <v-btn text>
+      <v-icon>mdi-cellphone-iphone</v-icon>
+      <h3>Contact</h3>
+    </v-btn>
+  </v-app-bar>
+</template>
+<style lang="scss">
+#profile-pic {
+  border-radius: 50%;
+}
+
+#profile-name {
+  padding-left: 10px;
+}
+</style>
+
+<script lang="ts">
+import Vue from 'vue';
+import Component from 'vue-class-component';
+
+@Component({})
+export default class Header extends Vue {}
+</script>
