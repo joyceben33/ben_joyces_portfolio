19615ae110db6dbf324c3a71c84219a669a988f5
BENZABOSS <joyceben33@gmail.com>
2020-12-02 15:32:01 -0500

    adjusted layout of content in about section.

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,14 +1,16 @@
 <template>
   <section id="about">
     <v-container>
-      <v-row justify="center">
+      <v-row>
         <v-col class="text-center" cols="12" md="12" lg="12" xl="12">
           <h2>About Me</h2>
         </v-col>
-        <v-col cols="12" lg="6" xl="6">
+      </v-row>
+      <v-row justify="center" align="center">
+        <v-col align-self="center" cols="12" sm="12" md="12" lg="4" xl="2">
           <v-img id="about-profile-img" :src="require('../assets/bnw_profile.jpeg')" max-width="350px"></v-img>
         </v-col>
-        <v-col cols="12" lg="6" xl="6">
+        <v-col align-self="center" cols="12" sm="12" md="12" lg="6" xl="6">
           <p>
             I have always been fascinated by technology and became a huge Apple fan-boy exemplified by jailbreaking my
             iPhone when I was 12. My two older brothers created a technology-focused startup that only furthered my
