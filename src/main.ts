c95fc82e0c2ebcac15b0b36465d496072946548c
BENZABOSS <joyceben33@gmail.com>
2020-12-02 15:31:40 -0500

    Added font styles and page section styling.

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import VueTyperPlugin from 'vue-typer';
 import App from './App.vue';
 import vuetify from './plugins/vuetify';
 
+import './assets/style/app.scss';
+
 Vue.config.productionTip = false;
 
 Vue.use(VueTyperPlugin);
