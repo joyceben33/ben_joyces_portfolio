54807fed904137700797d323c0969a7ce6d294b1
BENZABOSS <joyceben33@gmail.com>
2020-12-07 16:26:09 -0500

    added mobile nav for mobile screen sizes.

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -13,19 +13,53 @@
     <h1 id="profile-name">Ben Joyce</h1>
 
     <v-spacer></v-spacer>
-
-    <v-btn @click="scrollTo('#about')" text>
-      <v-icon>mdi-face</v-icon>
-      <h3>About</h3>
-    </v-btn>
-    <v-btn @click="scrollTo('#portfolio')" text>
-      <v-icon>mdi-laptop</v-icon>
-      <h3>Portfolio</h3>
-    </v-btn>
-    <v-btn @click="scrollTo('#contact')" text>
-      <v-icon>mdi-cellphone-iphone</v-icon>
-      <h3>Contact</h3>
-    </v-btn>
+    <div id="desktopMenu" class="hidden-md-and-down">
+      <v-btn @click="scrollTo('#about')" text>
+        <v-icon>mdi-face</v-icon>
+        <h3>About</h3>
+      </v-btn>
+      <v-btn @click="scrollTo('#portfolio')" text>
+        <v-icon>mdi-laptop</v-icon>
+        <h3>Portfolio</h3>
+      </v-btn>
+      <v-btn @click="scrollTo('#contact')" text>
+        <v-icon>mdi-cellphone-iphone</v-icon>
+        <h3>Contact</h3>
+      </v-btn>
+    </div>
+    <!-- mobile menu below -->
+    <div class="hidden-lg-and-up">
+      <v-app-bar-nav-icon default="mdiMenu" @click.stop="toggleMobileDrawer()"></v-app-bar-nav-icon>
+      <v-overlay @click.native="toggleMobileDrawer()" :value="mobileDrawer"></v-overlay>
+      <v-navigation-drawer v-model="mobileDrawer" app fixed hide-overlay right>
+        <v-list>
+          <v-list-item @click="scrollTo('#about')">
+            <v-list-item-icon>
+              <v-icon>mdi-face</v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title>About</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item @click="scrollTo('#portfolio')">
+            <v-list-item-icon>
+              <v-icon>mdi-laptop</v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title>Portfolio</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item @click="scrollTo('#contact')">
+            <v-list-item-icon>
+              <v-icon>mdi-cellphone-iphone</v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title>Contact</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+    </div>
   </v-app-bar>
 </template>
 <style lang="scss">
@@ -50,18 +84,40 @@
     }
   }
 }
+
+.v-overlay {
+  height: 100vh;
+}
 </style>
 
 <script lang="ts">
 import Vue from 'vue';
 import Component from 'vue-class-component';
+import { mdiMenu } from '@mdi/js';
 
 @Component({
   components: {},
 })
 export default class Header extends Vue {
+  mdiMenu = mdiMenu;
+
+  mobileDrawer: boolean | null = null;
+
   scrollTo(id: string) {
+    this.toggleMobileDrawer();
     this.$vuetify.goTo(id, { duration: 1000, offset: -25, easing: 'easeInOutCubic' });
+  }
+
+  toggleMobileDrawer() {
+    this.mobileDrawer = !this.mobileDrawer;
+    const bodyTag = document.getElementsByTagName('body')[0];
+    if (this.mobileDrawer) {
+      bodyTag.style.overflow = 'hidden';
+      bodyTag.style.height = '100vh';
+    } else {
+      bodyTag.style.removeProperty('overflow');
+      bodyTag.style.removeProperty('height');
+    }
   }
 }
 </script>
