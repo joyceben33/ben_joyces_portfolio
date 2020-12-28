edba21f5135b29cb733865e17183cf19b5cf28c7
BENZABOSS <joyceben33@gmail.com>
2020-12-28 16:09:13 -0500

    Added padding to v-icon within button and resized icon.

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,25 +1,23 @@
 <template>
   <section id="contact">
-    <address>
-      <v-container>
-        <v-row class="d-flex align-start">
-          <v-col class="text-center" cols="12" md="12" lg="12" xl="12">
-            <h2>Contact</h2>
-          </v-col>
-        </v-row>
+    <v-container>
+      <v-row class="d-flex align-start">
+        <v-col class="text-center" cols="12" md="12" lg="12" xl="12">
+          <h2>Contact</h2>
+        </v-col>
+      </v-row>
 
-        <div class="d-flex justify-center flex-wrap mx-n2">
-          <v-btn class="text-decoration-none ma-2" href="tel:3364550205" elevation="2" rounded large>
-            <v-icon large>{{ mdiPhone }}</v-icon>
-            336&centerdot;455&centerdot;0205
-          </v-btn>
-          <v-btn class="text-decoration-none ma-2" href="mailto:joyceben33@gmail.com" elevation="2" rounded large>
-            <v-icon large>{{ mdiEmail }}</v-icon>
-            joyceben33@gmail.com
-          </v-btn>
-        </div>
-      </v-container>
-    </address>
+      <div class="d-flex justify-center flex-wrap mx-n2">
+        <v-btn class="text-decoration-none ma-2" href="tel:3364550205" elevation="2" rounded large>
+          <v-icon class="pr-2">{{ mdiPhone }}</v-icon>
+          336&centerdot;455&centerdot;0205
+        </v-btn>
+        <v-btn class="text-decoration-none ma-2" href="mailto:joyceben33@gmail.com" elevation="2" rounded large>
+          <v-icon class="pr-2">{{ mdiEmail }}</v-icon>
+          joyceben33@gmail.com
+        </v-btn>
+      </div>
+    </v-container>
   </section>
 </template>
 
