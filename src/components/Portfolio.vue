44200ae9055a4060fa0f7669f2d2da79cca1fc7a
BENZABOSS <joyceben33@gmail.com>
2021-01-07 09:22:38 -0500

    Updated ppp project description.

--- src/components/Portfolio.vue
+++ src/components/Portfolio.vue
@@ -75,7 +75,8 @@ export default class Portfolio extends Vue {
     projects: [
       {
         project_name: 'Pomona Pipe Products',
-        project_description: `In my postion as a contract full-stack developer, another developer and I architected and engineered a workflow for updating the company's website.`,
+        project_description:
+          "Pomona Pipe Products, Inc. is a civil engineering firm specializing in bridges and environmental construction. The company services a wide range of private businesses, municipalities, and state Departments of Transportation. In my role as a full-stack developer, I have contributed to laying the groundwork for the company’s tech infrastructure. Pomona’s infrastructure consists of a Node API, AWS S3 Buckets, AWS CloudFront CDN, a Headless CMS, Algolia Search Engine, and a server-side rendering web application. In addition I have also helped with the company's SEO on Google and Bing as well as tweaked info on their Google My Business account.",
         preview_img: 'pomona_screenshot.png',
         deploy_link: 'https://www.pomonasupply.com/',
         repo_link: 'https://github.com/pomona-pipe/pomona-site-v2',
